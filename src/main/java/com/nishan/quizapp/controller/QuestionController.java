1c6c6fb43fd00350cd86cd647885fb301c15269e
Nishan Shyamika <nishan.18@itfac.mrt.ac.lk>
2023-11-20 23:43:20 +0530

    ResponceEntity and Exception Handling

--- src/main/java/com/nishan/quizapp/controller/QuestionController.java
+++ src/main/java/com/nishan/quizapp/controller/QuestionController.java
@@ -3,7 +3,7 @@ package com.nishan.quizapp.controller;
 import com.nishan.quizapp.model.Question;
 import com.nishan.quizapp.service.QuestionService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,17 +16,17 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestions(){
+    public ResponseEntity<List<Question>> getAllQuestions(){
         return questionService.getAllQuestions();
     }
 
     @GetMapping("category/{category}")
-    public List<Question> getQuestionsByCategory(@PathVariable String category){
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category){
         return questionService.getQuestionsByCategory(category);
     }
 
     @PostMapping("add")
-    public String addQuestion(@RequestBody Question question){
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
         return questionService.addQuestion(question);
     }
 }
--- src/main/java/com/nishan/quizapp/service/QuestionService.java
+++ src/main/java/com/nishan/quizapp/service/QuestionService.java
@@ -3,8 +3,11 @@ package com.nishan.quizapp.service;
 import com.nishan.quizapp.dao.QuestionDao;
 import com.nishan.quizapp.model.Question;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,16 +15,26 @@ import java.util.List;
 public class QuestionService {
 
     private final QuestionDao questionDao;
-    public List<Question> getAllQuestions(){
-        return questionDao.findAll();
+    public ResponseEntity<List<Question>> getAllQuestions(){
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public List<Question> getQuestionsByCategory(String category){
-        return questionDao.findByCategory(category);
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category){
+        try {
+            return new ResponseEntity<>(questionDao.findByCategory(category), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public String addQuestion(Question question) {
+    public ResponseEntity<String> addQuestion(Question question) {
         questionDao.save(question);
-        return "success";
+        return new ResponseEntity<>("success",HttpStatus.CREATED);
     }
 }
